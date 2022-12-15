7db0ab6d7cf27e1787e42c03999e0b3e90a535c9
aashayk18 <aashayk100@gmail.com>
2022-12-15 18:35:26 +0530

    initial commit

--- IOTwo.java
+++ IOTwo.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 import java.lang.String;
 
 public class IOTwo {
-
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
         int myInt = sc.nextInt();
         double myDouble = sc.nextDouble();
