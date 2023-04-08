4a2bd4324dd13a61a15e22eb8c72e7ecdfc9c32b
aashayk18 <aashayk100@gmail.com>
2023-04-08 18:32:07 +0530

    initial commit

--- Anagrams.java
+++ Anagrams.java
@@ -1,12 +1,13 @@
+public class Anagrams {
+    public static void main(String[] args) {
 
-
+    }
     static boolean isAnagram(String a, String b) {
-        // Complete the function
         char[] A = a.toUpperCase().toCharArray();
         char[] B = b.toUpperCase().toCharArray();
-        
+
         java.util.Arrays.sort(A);
-        java.util.Arrays.sort(B);        
+        java.util.Arrays.sort(B);
         return new String(A).equals(new String(B)) ? true : false;
     }
-
+}
