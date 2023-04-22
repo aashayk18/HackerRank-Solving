24592c7dd85901744fc6485d297c6121f7b01f59
aashayk18 <aashayk100@gmail.com>
2023-04-22 18:14:38 +0530

    initial commit

--- IntToStr.java
+++ IntToStr.java
@@ -1,10 +1,8 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
-
+public class IntToStr {
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String s = Integer.toString(n);
