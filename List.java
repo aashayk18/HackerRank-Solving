8cdfaffa69d9fafc275fea6e6063724b27acada3
aashayk18 <aashayk100@gmail.com>
2023-04-25 18:22:44 +0530

    initial commit

--- List.java
+++ List.java
@@ -2,27 +2,16 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class List {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
-
         ArrayList<Integer> L = new ArrayList<>();
-
         for (int i = 0; i < N; i++) {
-
             L.add(sc.nextInt());
-
         }
-
         int Q = sc.nextInt();
-
         for (int j = 0; j < Q; j++) {
-
             String s = sc.next();
-
             if (s.equals("Insert")) {
                 L.add(sc.nextInt(), sc.nextInt());
             } else if (s.equals("Delete")) {
