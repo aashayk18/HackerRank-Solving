4c3b69830edda4281e3147e0520704f5f89429b2
aashayk18 <aashayk100@gmail.com>
2023-05-17 16:49:48 +0530

    day 25

---
+++ StrRev.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class StrRev {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String rev = "";
+        
+        for (int i = A.length()-1; i>=0; i--) {
+            rev += A.charAt(i);
+        }
+        System.out.println(A.compareTo(rev)==0?"Yes":"No");
+    }
+}
+
+
+
