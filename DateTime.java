67c114ff568a81f2d6d3b530f8db648f6a99078b
aashayk18 <aashayk100@gmail.com>
2023-06-13 18:13:40 +0530

    initial commit

--- DateTime.java
+++ DateTime.java
@@ -13,11 +13,9 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 class Result {
-
     public static String findDay(int month, int day, int year) {
         return LocalDate.of(year, month, day).getDayOfWeek().toString();
     }
-
 }  
 
 public class DateTime {
@@ -28,11 +26,8 @@ public class DateTime {
         String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
         int month = Integer.parseInt(firstMultipleInput[0]);
-
         int day = Integer.parseInt(firstMultipleInput[1]);
-
         int year = Integer.parseInt(firstMultipleInput[2]);
-
         String res = Result.findDay(month, day, year);
 
         bufferedWriter.write(res);
