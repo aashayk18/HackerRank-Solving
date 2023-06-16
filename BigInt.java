105489f6dadb1ddf21360b7b5f218b8221aadddc
aashayk18 <aashayk100@gmail.com>
2023-06-16 17:07:58 +0530

    day 25

--- BigInt.java
+++ BigInt.java
@@ -7,7 +7,7 @@ import java.util.regex.*;
 public class BigInt {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
         Scanner sc = new Scanner(System.in);
         BigInteger a = sc.nextBigInteger();
         BigInteger b = sc.nextBigInteger();
